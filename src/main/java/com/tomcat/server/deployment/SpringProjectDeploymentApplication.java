920ad1ecc4ff768c413c5326f91e7a6b88dbe345
99sourav <address.sourav@gmail.com>
2024-12-30 17:34:24 +0530

    Update some changes.

--- src/main/java/com/tomcat/server/deployment/SpringProjectDeploymentApplication.java
+++ src/main/java/com/tomcat/server/deployment/SpringProjectDeploymentApplication.java
@@ -3,20 +3,21 @@ package com.tomcat.server.deployment;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Date;
 
 @SpringBootApplication
-public class SpringProjectDeploymentApplication implements CommandLineRunner {
+public class SpringProjectDeploymentApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectDeploymentApplication.class, args);
 	}
 
-
 	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("executing the job " + new Date());
-		System.out.println("Build Successful");
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(SpringProjectDeploymentApplication.class);
 	}
+
 }

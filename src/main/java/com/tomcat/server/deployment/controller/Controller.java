8a1ddd4e39ffd9dae8f6a52893e9f2abfe1d4926
Sourav Patra <88657830+99sourav@users.noreply.github.com>
2025-01-28 13:59:29 +0530

    Update Controller.java

--- src/main/java/com/tomcat/server/deployment/controller/Controller.java
+++ src/main/java/com/tomcat/server/deployment/controller/Controller.java
@@ -3,7 +3,7 @@ package com.tomcat.server.deployment.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Controller {
     @PostMapping("/get")
     public String fetchData(){
